82dd18fe9db887ebea8851bee23a3f7c68104085
Nicholas Engelking <moblin88@me.com>
2026-08-24 20:37:12 -0400

    Added Gaussian and Laplace kernels

--- src/KernelForecasting.jl
+++ src/KernelForecasting.jl
@@ -1,5 +1,29 @@
 module KernelForecasting
 
-# Write your package code here.
+export GaussianKernel, LaplaceKernel
+
+struct GaussianKernel{T<:Real}
+    σ²::T
+end
+
+function (k::GaussianKernel)(x, y)
+    s = zero(real(promote_type(eltype(x), eltype(y))))
+    for (u,v) in zip(x, y)
+        s += abs2(u - v)
+    end
+    return exp(-s/(2*k.σ²))
+end
+
+struct LaplaceKernel{T<:Real}
+    σ::T
+end
+
+function (k::LaplaceKernel)(x, y)
+    s = zero(real(promote_type(eltype(x), eltype(y))))
+    for (u,v) in zip(x, y)
+        s += abs(u - v)
+    end
+    return exp(-s/(2*k.σ/sqrt(π)))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using KernelForecasting
 using Test
 
 @testset "KernelForecasting.jl" begin
-    # Write your tests here.
+    @testset "GaussianKernel" begin
+        k = GaussianKernel(1.5)
+        x = [1.0, 2.0]
+        y = [2.0, 2.0]
+        @test k(x, y) ≈ exp(-1/(2*1.5))
+    end
+
+    @testset "LaplaceKernel" begin
+        k = LaplaceKernel(1.5)
+        x = [1.0, 2.0]
+        y = [2.0, 3.0]
+        @test k(x, y) ≈ exp(-2/(2*1.5/sqrt(π)))
+    end
 end
