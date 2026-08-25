95c68d941a736973d6af2e900d4340413d63b784
Nicholas Engelking <moblin88@me.com>
2026-08-24 23:50:59 -0400

    Added kernel precomputation

--- src/KernelForecasting.jl
+++ src/KernelForecasting.jl
@@ -1,5 +1,6 @@
 module KernelForecasting
 
+using LinearAlgebra
 export GaussianKernel, LaplaceKernel
 
 struct GaussianKernel{T<:Real}
@@ -24,6 +25,24 @@ function (k::LaplaceKernel)(x, y)
         s += abs(u - v)
     end
     return exp(-s/(2*k.σ/sqrt(π)))
+end
+
+function precompute_kernels(k, data, lags)
+    Base.require_one_based_indexing(data)
+    n = size(data, 1)
+    1<=lags <= n || throw(ArgumentError("lags must be between 1 and the number of rows in data"))
+    mem1 = @views k(data[1, :], data[1, :])
+    mem = Memory{typeof(mem1)}(undef, lags*(n-1) + n)
+    # fill the first matrix
+    mem[1] = mem1
+    for j in 2:lags, i in 1:j
+        mem[(j-1) * lags + i] = @views k(data[i, :], data[j, :])
+    end
+    #fill the remaining columns
+    for j in lags+1:n, i in j-lags:j
+        mem[(j-1) * lags + i] = @views k(data[i, :], data[j, :])
+    end
+    return mem
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using KernelForecasting
 using Test
+using LinearAlgebra
 
 @testset "KernelForecasting.jl" begin
     @testset "GaussianKernel" begin
@@ -14,5 +15,24 @@ using Test
         x = [1.0, 2.0]
         y = [2.0, 3.0]
         @test k(x, y) ≈ exp(-2/(2*1.5/sqrt(π)))
+    end
+
+    @testset "precompute_kernels" begin
+        data = [1.0 2.0; 3.0 4.0; 5.0 6.0; 7.0 8.0; 9.0 10.0]
+        lags = 3
+        n = size(data, 1)
+        k = GaussianKernel(1.5)
+        pk = KernelForecasting.precompute_kernels(k, data, lags)
+        @test length(pk) == lags^2 + (n-lags)*(lags+1)
+        for (i,j) in zip(range(1; length = n - lags), range(1; length = n - lags, step = lags+1))
+            kmatrix = [@views k(data[x, :], data[y, :]) for x in range(i; length = lags), y in range(i; length = lags)]
+            kmatrixview = Symmetric(reshape(view(pk, range(j; length = lags^2)),lags,lags),:U)
+            @test kmatrixview ≈ kmatrix
+        end
+        for (i,j) in zip(range(stop = n, length = n - lags),range(stop=length(pk)-lags, length = n - lags, step = lags + 1))
+            kvector = [@views k(data[x, :], data[i, :]) for x in range(stop = i-1, length = lags)]
+            kvectorview = view(pk, range(j; length = lags))
+            @test kvectorview ≈ kvector
+        end
     end
 end
